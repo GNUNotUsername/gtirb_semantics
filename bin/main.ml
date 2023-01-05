56d9d573395389d9d7142b165ef7c3d00ae93bbd
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-01-05 11:23:33 +1000

    Dumps out machine code contents

--- bin/main.ml
+++ bin/main.ml
@@ -11,9 +11,14 @@ let () =
     let res = really_input_string ic (len - 8)  in
     close_in ic; 
     res
-  in 
+  in
   
-  (* Replace with IR.Modules.Sections -> isolate .text -> text.contents *)
+  (* TODO make N dimensional map function later, this map (map (map )) business is silly
+  This doesn't work
+  let rec map_nd n f l =
+  if n = 1 then map f l
+  else map_nd (n - 1) (map f) l*)
+
   let raw     = Runtime'.Reader.create bytes                      in
   let gtirb   = Gtirb_semantics.IR.Gtirb.Proto.IR.from_proto raw  in
   let result  = (
@@ -21,9 +26,13 @@ let () =
     | Ok ir   -> ir.modules
     | Error e -> failwith (Printf.sprintf "Could not reply request: %s" (Ocaml_protoc_plugin.Result.show_error e))
   ) in
-  let sections (modul : Gtirb_semantics.Module.Gtirb.Proto.Module.t) = modul.sections             in
-  let all_sects = map sections result                                                             in
-  (*let is_text (section : Gtirb_semantics.Section.Gtirb.Proto.Section.t) = section.name = ".text"  in
-  let all_texts = map (filter is_text) all_sects                                                  in*)
-  let take_names (section : Gtirb_semantics.Section.Gtirb.Proto.Section.t) = section.name         in
-  let names = map (map take_names) all_sects in iter (iter print_endline) names
+  let sections (modul : Gtirb_semantics.Module.Gtirb.Proto.Module.t) = modul.sections               in
+  let all_sects = map sections result                                                               in
+  let is_text (section : Gtirb_semantics.Section.Gtirb.Proto.Section.t) = section.name = ".text"    in
+  let all_texts = map (filter is_text) all_sects                                                    in
+  let sect_ivals (section : Gtirb_semantics.Section.Gtirb.Proto.Section.t) = section.byte_intervals in
+  let intervals = map (map sect_ivals) all_texts                                                    in
+  let int_conts (ival : Gtirb_semantics.ByteInterval.Gtirb.Proto.ByteInterval.t) = ival.contents    in
+  let raw_conts = map (map (map int_conts)) intervals                                               in
+  let hex_conts = map (map (map Hexstring.encode)) raw_conts                                        in
+  iter (iter (iter print_endline)) hex_conts
