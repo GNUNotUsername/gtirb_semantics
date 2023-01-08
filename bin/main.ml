12f438136c93edd074abea3b62f2a288df92a1b2
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-01-09 09:24:47 +1000

    Having trouble with resolving blocks, might revert to this

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,15 @@
 open Ocaml_protoc_plugin.Runtime
 open List
 
+type rectified_block = {
+  uuid    : bytes;
+  size    : int;
+  offset  : int;
+}
+
 let () = 
 
+  let death = "Could not reply request: "     in
   (* Read bytes from the file, skip first 8*) 
   let bytes = 
     let ic  = open_in Sys.argv.(1)              in 
@@ -12,24 +19,36 @@ let () =
     close_in ic; 
     res
   in
-  
-  (* TODO make N dimensional map function later, this map (map (map )) business is silly
-  This doesn't work
-  let rec map_nd n f l =
-  if n = 1 then map f l
-  else map_nd (n - 1) (map f) l*)
+
+  (* TODO Make an N-dimensional map function *)
 
   let raw     = Runtime'.Reader.create bytes                      in
   let gtirb   = Gtirb_semantics.IR.Gtirb.Proto.IR.from_proto raw  in
   let result  = (
     match gtirb with
     | Ok ir   -> ir.modules
-    | Error e -> failwith (Printf.sprintf "Could not reply request: %s" (Ocaml_protoc_plugin.Result.show_error e))
+    | Error e -> failwith (Printf.sprintf "%s%s" death (Ocaml_protoc_plugin.Result.show_error e))
   ) in
-  let all_sects = map (fun (m : Gtirb_semantics.Module.Gtirb.Proto.Module.t) -> m.sections) result                          in
-  let all_texts = map (filter (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.name = ".text")) all_sects      in
-  let intervals = map (map (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.byte_intervals)) all_texts         in (* 2D list of all byte intervals *)
-  let ival_blks = map (map (map (fun (i : Gtirb_semantics.ByteInterval.Gtirb.Proto.ByteInterval.t) -> i.blocks))) intervals in
-  let get_offsets (block : Gtirb_semantics.ByteInterval.Gtirb.Proto.Block.t) = block.offset                                 in
-  let bsizes    = map (map (map (map get_offsets))) ival_blks                                                               in
-  iter (iter (iter (iter (Printf.printf "%d\n")))) bsizes
+  (* there's gotta be a better way to do this... blame the lack of functional programming experience... *)
+  let all_sects = map (fun (m : Gtirb_semantics.Module.Gtirb.Proto.Module.t) -> m.sections) result                            in
+  let all_texts = map (filter (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.name = ".text")) all_sects        in
+  let intervals = map (map (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.byte_intervals)) all_texts           in (* 2D list of all byte intervals *)
+  let contents  = map (map (map (fun (i : Gtirb_semantics.ByteInterval.Gtirb.Proto.ByteInterval.t) -> i.contents))) intervals in
+  let ival_blks = map (map (map (fun (i : Gtirb_semantics.ByteInterval.Gtirb.Proto.ByteInterval.t) -> i.blocks))) intervals   in
+  let offsets   = map (map (map (map (fun (b : Gtirb_semantics.ByteInterval.Gtirb.Proto.Block.t) -> b.offset)))) ival_blks    in
+  let poly_blks = map (map (map (map (fun (b : Gtirb_semantics.ByteInterval.Gtirb.Proto.Block.t) -> b.value))))  ival_blks    in
+  let rectify   = function
+    | `Code (c : Gtirb_semantics.CodeBlock.Gtirb.Proto.CodeBlock.t) -> {uuid = c.uuid; size = c.size; offset = 0}
+    | `Data (d : Gtirb_semantics.DataBlock.Gtirb.Proto.DataBlock.t) -> {uuid = d.uuid; size = d.size; offset = 0}
+    | _ -> {uuid = Bytes.empty; size = 0; offset = 0}
+  in
+  let r_blocks  = map (map (map (map rectify))) poly_blks in
+  (* This is pain
+  let add_offsets rectified offsets =
+    match rectified with
+    | []      -> []
+    | h::t    -> (match offsets with
+                  | [] -> []
+                  | g :: s -> (record_zip g h) :: )
+    | [h::t]  ->
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*)
