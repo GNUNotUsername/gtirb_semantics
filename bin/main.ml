f062b0f4e242b20c7f025aae3fe2d21e6f3cd8a4
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-01-09 14:01:58 +1000

    Endianness correction almost works (tm)
    Don't build this; it won't run

--- bin/main.ml
+++ bin/main.ml
@@ -5,6 +5,7 @@ open List
 type rectified_block = {
   uuid      : bytes;
   contents  : bytes;
+  opcodes   : bytes list;
   (* Whatever ASLi gives back goes here later*)
   size      : int;
   offset    : int;
@@ -14,6 +15,8 @@ type content_block = {
   block    : Gtirb_semantics.ByteInterval.Gtirb.Proto.Block.t;
   raw      : bytes;
 }
+
+let opcode_length = 4;;
 
 let () = 
 
@@ -35,19 +38,18 @@ let () =
   let map3 f l = map (map (map f)) l        in
   let map4 f l = map (map (map (map f))) l  in
 
-  let rblock sz id = {uuid = id; contents = empty; size = sz; offset = 0} in
+  let rblock sz id = {uuid = id; contents = empty; opcodes = []; size = sz; offset = 0} in
 
   let raw     = Runtime'.Reader.create bytes                      in
   let gtirb   = Gtirb_semantics.IR.Gtirb.Proto.IR.from_proto raw  in
-  let result  = (
+  let modules  = (
     match gtirb with
     | Ok ir   -> ir.modules
     | Error e -> failwith (Printf.sprintf "%s%s" death (Ocaml_protoc_plugin.Result.show_error e))
   ) in
-  let all_sects = map (fun (m : Gtirb_semantics.Module.Gtirb.Proto.Module.t) -> m.sections) result                      in
+  let all_sects = map (fun (m : Gtirb_semantics.Module.Gtirb.Proto.Module.t) -> m.sections) modules                     in
   let all_texts = map (filter (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.name = ".text")) all_sects  in
   let intervals = map2 (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.byte_intervals) all_texts          in (* 2D list of all byte intervals *)
-  (*let contents  = map3 (fun (i : Gtirb_semantics.ByteInterval.Gtirb.Proto.ByteInterval.t) -> i.contents) intervals      in*)
   let ival_blks = map3 (fun (i : Gtirb_semantics.ByteInterval.Gtirb.Proto.ByteInterval.t)
       -> map (fun b -> {block = b; raw = i.contents}) i.blocks) intervals in
   let rectify   = function
@@ -55,8 +57,29 @@ let () =
     | `Data (d : Gtirb_semantics.DataBlock.Gtirb.Proto.DataBlock.t) -> rblock d.size d.uuid
     | _ -> rblock 0 empty
   in
-  (*let offsets   = map4 (fun (b : Gtirb_semantics.ByteInterval.Gtirb.Proto.Block.t) -> b.offset) ival_blks in*)
   let poly_blks = map4 (fun b -> {{(rectify b.block.value) with offset = b.block.offset} with contents = b.raw}) ival_blks  in
   let trimmed   = map4 (fun b -> {b with contents = sub b.contents b.offset b.size}) poly_blks                              in
-  let visible   = map4 (fun b -> Hexstring.encode b.contents) trimmed                                                       in
-  iter (iter (iter (iter print_endline))) visible
+  (* There's gotta be a better way than this *)
+  let b_tl op n = sub op n ((Bytes.length op - n))  in
+  let b_hd op n = sub op 0 n                        in
+  let rec cut_ops contents =
+    if Bytes.length contents <= opcode_length then [contents]
+    else ((b_hd contents opcode_length) :: cut_ops (b_tl contents opcode_length)) in
+  let op_cuts   = map4 (fun b -> {b with opcodes = cut_ops b.contents}) trimmed in
+  let need_flip = map (fun (m : Gtirb_semantics.Module.Gtirb.Proto.Module.t)
+      -> m.byte_order = Gtirb_semantics.Module.Gtirb.Proto.ByteOrder.LittleEndian) modules  in
+  let rec endian_reverse opcode = cat (endian_reverse (b_tl opcode 1)) (b_hd opcode 1)      in
+  let flip_opcodes block = {block with opcodes = map endian_reverse block.opcodes}          in
+  (* Bless me father for I have sinned *)
+  let rec fix_endianness flips blocks = (
+      if (hd flips) then map3 flip_opcodes (hd blocks) else (hd blocks)
+    ) :: (fix_endianness (tl (flips)) (tl blocks))
+  in
+  let end_fixed = fix_endianness need_flip op_cuts  in
+  let rec pp_ops ops = 
+  match ops with
+  | [] -> print_endline ""
+  | h::t -> (print_bytes h; pp_ops t); in
+  let dump_ops b = pp_ops b.opcodes in
+
+  iter (iter (iter (iter dump_ops))) end_fixed
