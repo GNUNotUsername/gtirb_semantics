f461b942edb5b23b2bebc7f3655cf88f9b7f46c9
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-01-10 11:14:26 +1000

    Fixed bad imports
    Still don't build this

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,10 @@
 open Ocaml_protoc_plugin.Runtime
+open Gtirb_semantics.IR.Gtirb.Proto
+open Gtirb_semantics.ByteInterval.Gtirb.Proto
+open Gtirb_semantics.Module.Gtirb.Proto
+open Gtirb_semantics.Section.Gtirb.Proto
+open Gtirb_semantics.CodeBlock.Gtirb.Proto
+open Gtirb_semantics.DataBlock.Gtirb.Proto
 open LibASL
 open Bytes
 open List
@@ -14,7 +20,7 @@ type rectified_block = {
 }
 
 type content_block = {
-  block   : Gtirb_semantics.ByteInterval.Gtirb.Proto.Block.t;
+  block   : Block.t;
   raw     : bytes;
   address : int; 
 }
@@ -33,10 +39,11 @@ let () =
 
   let rblock sz id = {uuid = id; contents = empty; opcodes = []; asts = []; address = 0; offset = 0; size = sz; } in
   
-  (* Byte manipulation convenience functions *)
-  let len = Bytes.length in
-  let b_tl op n = Bytes.sub op n (len op - n) in
-  let b_hd op n = Bytes.sub op 0 n            in
+  (* Byte & array manipulation convenience functions *)
+  let len         = Bytes.length                                        in
+  let b_tl op n   = Bytes.sub op n (len op - n)                         in
+  let b_hd op n   = Bytes.sub op 0 n                                    in
+  let asbtol a i  = Array.to_list (Array.sub a i (Array.length a - i))  in 
 
   (* Main *)
   print_endline "";
@@ -52,23 +59,23 @@ let () =
   in
 
   (* Pull out interesting code bits *)
-  let raw     = Runtime'.Reader.create bytes                      in
-  let gtirb   = Gtirb_semantics.IR.Gtirb.Proto.IR.from_proto raw  in
+  let raw     = Runtime'.Reader.create bytes  in
+  let gtirb   = IR.from_proto raw             in
   let modules = (
     match gtirb with
     | Ok ir   -> ir.modules
     | Error e -> failwith (Printf.sprintf "%s%s" "Could not reply request: " (Ocaml_protoc_plugin.Result.show_error e))
   ) in
-  let all_sects = map (fun (m : Gtirb_semantics.Module.Gtirb.Proto.Module.t) -> m.sections) modules                     in
-  let all_texts = map (filter (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.name = ".text")) all_sects  in
-  let intervals = map2 (fun (s : Gtirb_semantics.Section.Gtirb.Proto.Section.t) -> s.byte_intervals) all_texts          in (* 2D list of all byte intervals *)
-  let ival_blks = map3 (fun (i : Gtirb_semantics.ByteInterval.Gtirb.Proto.ByteInterval.t)
+  let all_sects = map (fun (m : Module.t) -> m.sections) modules                    in
+  let all_texts = map (filter (fun (s : Section.t) -> s.name = ".text")) all_sects  in
+  let intervals = map2 (fun (s : Section.t) -> s.byte_intervals) all_texts          in (* 2D list of all byte intervals *)
+  let ival_blks = map3 (fun (i : ByteInterval.t)
       -> map (fun b -> {block = b; raw = i.contents; address = i.address}) i.blocks) intervals in
 
   (* Resolve polymorphic block variants to isolate info we actually care about *)
   let rectify   = function
-    | `Code (c : Gtirb_semantics.CodeBlock.Gtirb.Proto.CodeBlock.t) -> rblock c.size c.uuid
-    | `Data (d : Gtirb_semantics.DataBlock.Gtirb.Proto.DataBlock.t) -> rblock d.size d.uuid (* Might just delete these *)
+    | `Code (c : CodeBlock.t) -> rblock c.size c.uuid
+    | `Data (d : DataBlock.t) -> rblock d.size d.uuid (* Might just delete these? *)
     | _ -> rblock 0 empty
   in
   let poly_blks   = map4 (fun b -> {{{(rectify b.block.value)
@@ -77,15 +84,15 @@ let () =
     with address  = b.address + b.block.offset}) ival_blks in
   
   (* Section up byte interval contents to their respective blocks and slice out individual opcodes *)
-  let trimmed   = map4 (fun b -> {b with contents = Bytes.sub b.contents b.offset b.size}) poly_blks                              in
+  let trimmed   = map4 (fun b -> {b with contents = Bytes.sub b.contents b.offset b.size}) poly_blks in
   let rec cut_ops contents =
     if len contents <= opcode_length then [contents]
     else ((b_hd contents opcode_length) :: cut_ops (b_tl contents opcode_length)) in
   let op_cuts   = map4 (fun b -> {b with opcodes = cut_ops b.contents}) trimmed   in
 
   (* Convert every opcode to big endianness *)
-  let need_flip = map (fun (m : Gtirb_semantics.Module.Gtirb.Proto.Module.t)
-      -> m.byte_order = Gtirb_semantics.Module.Gtirb.Proto.ByteOrder.LittleEndian) modules in
+  let need_flip = map (fun (m : Module.t)
+      -> m.byte_order = ByteOrder.LittleEndian) modules in
   let rec endian_reverse opcode = 
     if len opcode = 1 then opcode
     else cat (endian_reverse (b_tl opcode 1)) (b_hd opcode 1)                       in
@@ -105,9 +112,8 @@ let () =
   let end_fixed = fix_endianness need_flip op_cuts in
 
   (* Organise specs to allow for ASLi evaluation environment setup *)
-  let asb a i = Array.to_list (Array.sub a i (Array.length a - i))    in
   let p       = Sys.argv.(prelude_ind)                                in
-  let specs   = asb Sys.argv specs_start                              in
+  let specs   = asbtol Sys.argv specs_start                           in
   let prelude = LoadASL.read_file p true false                        in
   let mra     = map (fun t -> LoadASL.read_file t false false) specs  in
   let envinfo = concat (prelude :: mra)                               in
