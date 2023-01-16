473ca25735aec6c7cc580d5d7e64211396383e1b
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-01-16 11:15:12 +1000

    Improved function nesting

--- bin/main.ml
+++ bin/main.ml
@@ -48,8 +48,6 @@ let kv_pair       = ":"
 (*
 TODOS
 
-* Fix the n dimensional map thing ==> or just flatten the lists? UUIDs should take care of everything
-* Clean up the hd/tl variants thrown about everywhere
 * Condense the asli outputs
 *)
 
@@ -69,8 +67,8 @@ let () =
     opcodes   = [];
     address   = 0;
     offset    = 0;
-    size      = sz; }
-  in
+    size      = sz;
+  } in
   
   (* Byte & array manipulation convenience functions *)
   let len         = Bytes.length                                        in
@@ -90,68 +88,77 @@ let () =
   in
 
   (* Pull out interesting code bits *)
-  let raw     = Runtime'.Reader.create bytes  in
-  let gtirb   = IR.from_proto raw             in
-  let modules = (
-    match gtirb with
-    | Ok ir   -> ir.modules
-    | Error e -> failwith (Printf.sprintf "%s%s" "Could not reply request: " (Ocaml_protoc_plugin.Result.show_error e))
-  ) in
-  let all_sects = map (fun (m : Module.t) -> m.sections) modules                          in
-  let all_texts = map (filter (fun (s : Section.t) -> s.name = text)) all_sects           in
-  let intervals = map2 (fun (s : Section.t) -> s.byte_intervals) all_texts |> map flatten in
-  let ival_blks = map2 (fun (i : ByteInterval.t)
+  let modules =
+    let raw     = Runtime'.Reader.create bytes  in
+    let gtirb   = IR.from_proto raw             in
+      match gtirb with
+      | Ok ir   -> ir.modules
+      | Error e -> failwith (Printf.sprintf "%s%s" "Could not reply request: " (Ocaml_protoc_plugin.Result.show_error e))
+  in
+  let ival_blks =
+    let all_sects = map (fun (m : Module.t) -> m.sections) modules                          in
+    let all_texts = map (filter (fun (s : Section.t) -> s.name = text)) all_sects           in
+    let intervals = map2 (fun (s : Section.t) -> s.byte_intervals) all_texts |> map flatten in
+    map2 (fun (i : ByteInterval.t)
       -> map (fun b -> {block = b; raw = i.contents; address = i.address}) i.blocks) intervals
       |> map flatten
   in
 
   (* Resolve polymorphic block variants to isolate info we actually care about *)
-  let rectify   = function
-    | `Code (c : CodeBlock.t) -> rblock c.size c.uuid
-    | _                       -> rblock 0 empty
-  in
-  let poly_blks   = map2 (fun b -> {{{(rectify b.block.value)
-    with offset   = b.block.offset}
-    with contents = b.raw}
-    with address  = b.address + b.block.offset}) ival_blks
-  in
-  let codes_only  = map (filter (fun b -> b.size > 0)) poly_blks in
+  let codes_only  = 
+    let rectify   = function
+      | `Code (c : CodeBlock.t) -> rblock c.size c.uuid
+      | _                       -> rblock 0 empty
+    in
+    let poly_blks   = map2 (fun b -> {{{(rectify b.block.value)
+      with offset   = b.block.offset}
+      with contents = b.raw}
+      with address  = b.address + b.block.offset}) ival_blks
+    in 
+    map (filter (fun b -> b.size > 0)) poly_blks in
   
   (* Section up byte interval contents to their respective blocks and slice out individual opcodes *)
-  let trimmed   = map2 (fun b -> {b with contents = Bytes.sub b.contents b.offset b.size}) codes_only in
-  let rec cut_ops contents =
-    if len contents <= opcode_length then [contents]
-    else ((b_hd contents opcode_length) :: cut_ops (b_tl contents opcode_length)) in
-  let op_cuts   = map2 (fun b -> {b with opcodes = cut_ops b.contents}) trimmed   in
+  let op_cuts   =
+    let trimmed = map2 (fun b -> {b with contents = Bytes.sub b.contents b.offset b.size}) codes_only in
+    let rec cut_ops contents =
+      if len contents <= opcode_length then [contents]
+      else ((b_hd contents opcode_length) :: cut_ops (b_tl contents opcode_length))
+    in
+    map2 (fun b -> {b with opcodes = cut_ops b.contents}) trimmed
+  in
 
   (* Convert every opcode to big endianness *)
-  let need_flip = map (fun (m : Module.t)
-      -> m.byte_order = ByteOrder.LittleEndian) modules in
-  let rec endian_reverse opcode = 
-    if len opcode = 1 then opcode
-    else cat (endian_reverse (b_tl opcode 1)) (b_hd opcode 1)                       in
-  let flip_opcodes block = {block with opcodes = map endian_reverse block.opcodes}  in
-  (* This could probably be optimised *)
-  let rec fix_endianness flips blocks f =
-    (
-      match flips with
-      | []      -> []
-      | h :: _  -> (if h then map f (hd blocks) else (hd blocks))
-    ) :: (
-      match flips with
-      | []      -> []
-      | _ :: t  -> fix_endianness t (tl blocks) f
-    )
+  let blk_orded =
+    let need_flip = map (fun (m : Module.t)
+        -> m.byte_order = ByteOrder.LittleEndian) modules in
+    let rec endian_reverse opcode = 
+      if len opcode = 1 then opcode
+      else cat (endian_reverse (b_tl opcode 1)) (b_hd opcode 1)                       in
+    let flip_opcodes block = {block with opcodes = map endian_reverse block.opcodes}  in
+    (* This could probably be optimised *)
+    let rec fix_endianness flips blocks f =
+      (
+        match flips with
+        | []      -> []
+        | h :: _  -> (if h then map f (hd blocks) else (hd blocks))
+      ) :: (
+        match flips with
+        | []      -> []
+        | _ :: t  -> fix_endianness t (tl blocks) f
+      )
+    in
+    let end_fixed = fix_endianness need_flip op_cuts flip_opcodes in
+    fix_endianness need_flip end_fixed (fun b -> {b with opcodes = rev b.opcodes})
   in
-  let end_fixed = fix_endianness need_flip op_cuts flip_opcodes                                   in
-  let blk_orded = fix_endianness need_flip end_fixed (fun b -> {b with opcodes = rev b.opcodes})  in
 
   (* Organise specs to allow for ASLi evaluation environment setup *)
-  let prel    = Sys.argv.(prelude_ind)                                in
-  let specs   = asbtol Sys.argv specs_start                           in
-  let prelude = LoadASL.read_file prel true false                     in
-  let mra     = map (fun t -> LoadASL.read_file t false false) specs  in
-  let envinfo = concat (prelude :: mra)                               in
+  let envinfo =
+    let prel    = Sys.argv.(prelude_ind)                                in
+    let specs   = asbtol Sys.argv specs_start                           in
+    let prelude = LoadASL.read_file prel true false                     in
+    let mra     = map (fun t -> LoadASL.read_file t false false) specs  in
+    concat (prelude :: mra)
+  in
 
   (* Evaluate each opcode one by one with a new environment for each *)
   let to_asli op addr =
@@ -174,9 +181,11 @@ let () =
   let with_asts = map2 (fun b -> {auuid = b.ruuid; asts = (asts b.opcodes b.address envinfo); concat = ""}) blk_orded in
 
   (* Now massage asli outputs into a format which can be serialised and then deserialised by other tools *)
-  let l_to_s op d cl l = op ^ (String.concat d l) ^ cl                        in
-  let jsoned asts = map (l_to_s l_op l_dl l_cl) asts |> l_to_s l_op l_dl l_cl in
-  let json_asts   = map2 (fun b -> {b with concat = jsoned b.asts}) with_asts in
+  let json_asts   =
+    let l_to_s op d cl l = op ^ (String.concat d l) ^ cl                        in
+    let jsoned asts = map (l_to_s l_op l_dl l_cl) asts |> l_to_s l_op l_dl l_cl in
+    map2 (fun b -> {b with concat = jsoned b.asts}) with_asts
+  in
 
   (* Pair everything up with code block uuids *)
   let no_nops = map (filter (fun b -> String.length b.concat > noplen)) json_asts       in (* Comparing to [[]] not working? *)
