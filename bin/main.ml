c463c0ad49ca034f7ef6342a3f9ef10d8a5d5d9d
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-01-31 14:53:56 +1000

    Clean up asli outputs to make semantic restructuring less painful

--- bin/main.ml
+++ bin/main.ml
@@ -170,7 +170,27 @@ let () =
 
   (* Evaluate each opcode one by one with a new environment for each *)
   let to_asli op addr =
-    let p_raw a = Utils.to_string (Asl_parser_pp.pp_raw_stmt a) |> String.trim  in
+    let p_raw a = 
+      let rec swap_quotes s =
+        let slen = String.length s in
+        if slen = 0
+        then s
+        else (
+          let p = String.sub s 0 1 in
+          let q =
+              if p = "\n" 
+              then ","
+              else p
+          in 
+          q ^ swap_quotes (String.sub s 1 (slen - 1))
+        )
+      in 
+      let s = Utils.to_string (Asl_parser_pp.pp_raw_stmt a) |> String.trim |> swap_quotes in
+      (
+        print_endline s;
+        s
+      )
+    in
     let address = Some (string_of_int addr)                                     in
     let env     = Eval.build_evaluation_environment envinfo                     in
     let str     = hex ^ Hexstring.encode op                                     in 
@@ -178,7 +198,7 @@ let () =
     let ascii   = map p_raw res                                                 in
     let indiv s = init (String.length s) (String.get s) |> map (String.make 1)  in
     let joined  = map indiv ascii |>  map (String.concat "")                    in
-    map (fun s -> strung ^ s ^ strung) joined
+    map (fun s -> "'" ^ s ^ "'") joined
   in
   let rec asts opcodes addr envinfo =
     match opcodes with
